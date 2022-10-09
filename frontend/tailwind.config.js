80faa8dcc13ecd791bcda9821b91bb44e58777b9
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-10-09 12:19:45 -0500

    Creating a vue project and adding tailwind to it.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: '#1d1a53',
+        lightBlue: '#3dc7ee',
+      }
+    },
   },
   plugins: [],
 }
