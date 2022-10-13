51b784766f4716383b23a42a22e37f74458b35fc
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-10-13 17:38:47 +0330

    Add new fields to abstract user model.

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,6 +1,12 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(max_length=11, blank=True, null=True, verbose_name="Phone Number")
+    telegram_id = models.CharField(max_length=11, blank=True, null=True, verbose_name="Telegram ID")
+    prediction_token = models.CharField(max_length=100, blank=True, null=True, verbose_name="Prediction Token")
+    total_prediction_points = models.IntegerField(default=0, verbose_name="Total Prediction Points")
+    monthly_prediction_points = models.IntegerField(default=0, verbose_name="Monthly Prediction Points")
+    weekly_prediction_points = models.IntegerField(default=0, verbose_name="Weekly Prediction Points")
