a5b8d64bee4c3f033a7636ef25b944a2c1ac0e3d
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-10-13 17:31:45 +0330

    Creating account app and add an abstract user model.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'prediction.apps.PredictionConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -99,13 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'account.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
