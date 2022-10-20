05706742f7193bbebb45d4f836681792bfe9ab78
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-10-20 21:11:08 +0330

    ADD: Regex for finding fixture elements.

--- prediction/static/js/form_actions.js
+++ prediction/static/js/form_actions.js
@@ -1,4 +1,4 @@
 // Change number of forms in management form for validation the formsset in backend
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('id_form-TOTAL_FORMS').value = document.getElementsByName('fixture_id').length;
+    document.getElementById('id_form-TOTAL_FORMS').value = document.getElementsByName('form-\[[0-9]+\]-fixture_id').length;
 });
