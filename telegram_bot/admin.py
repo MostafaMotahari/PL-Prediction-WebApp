1a057e5360fa0c4cafb581403a311c5fe08c8cb5
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-10-21 22:23:53 +0330

    ADD: Telegram bot models to admin panel.

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,3 +1,7 @@
+from .models import LeagueModel, LoadingModel
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(LeagueModel)
+admin.site.register(LoadingModel)
+
