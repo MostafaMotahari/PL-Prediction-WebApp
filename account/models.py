1897eee383b0b77d75b1f054644ad71fbbfdf85e
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-12-30 15:05:51 +0330

    Fix some bugs.

--- account/models.py
+++ account/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=False, verbose_name="Username")
     phone_number = models.CharField(max_length=13, blank=True, null=True, verbose_name="Phone Number")
     card_number = models.CharField(max_length=16, blank=True, null=True, verbose_name="Card Number")
     telegram_id = models.CharField(max_length=11, blank=True, null=True, verbose_name="Telegram ID")
