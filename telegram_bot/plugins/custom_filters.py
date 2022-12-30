313b3447972a96aac9b3b73de69e82489369dec1
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-12-30 14:52:39 +0330

    Fix some bugs.

--- telegram_bot/plugins/custom_filters.py
+++ telegram_bot/plugins/custom_filters.py
@@ -20,9 +20,9 @@ admin_filter = filters.create(admin_filter)
 
 
 def banned_filter(_, __, message):
-    user = User.objects.get(telegram_id=message.from_user.id)
-
     try:
+        user = User.objects.get(telegram_id=message.from_user.id)
+
         if __.get_chat_member(config("MAIN_CHANNEL"), message.from_user.id):
             if user.status == "banned":
                 user.status = "user"
@@ -43,9 +43,6 @@ def banned_filter(_, __, message):
         )
         user.status = "banned"
         user.save()
-        return False
-        
-    except user.DoesNotExist:
         return False
 
     except Exception as e:
