9319326b24981c6e7f3d2c071e7103c44032a95c
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-01-27 22:37:28 +0330

    Unique id

--- account/models.py
+++ account/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     phone_number = models.CharField(max_length=13, blank=True, null=True, verbose_name="Phone Number")
     card_number = models.CharField(max_length=16, blank=True, null=True, verbose_name="Card Number")
-    telegram_id = models.CharField(max_length=11, blank=True, null=True, verbose_name="Telegram ID")
+    telegram_id = models.CharField(max_length=11, unique=True , blank=True, null=True, verbose_name="Telegram ID")
     status = models.CharField(max_length=10, default="user", verbose_name="Status")
     prediction_token = models.CharField(max_length=100, blank=True, null=True, verbose_name="Prediction Token")
     total_prediction_points = models.IntegerField(default=0, verbose_name="Total Prediction Points")
