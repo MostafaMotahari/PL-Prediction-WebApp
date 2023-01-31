c787bef00ec538bb665577c7bb4ad2d126825c09
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-01-31 17:04:30 +0330

    Add tournament model to admin panel

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'related_league_code', 'player_capacity', )
+
+
+admin.site.register(models.Tournament, TournamentAdmin)
