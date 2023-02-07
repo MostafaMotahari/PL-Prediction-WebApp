2535edaa5ca4d0acaa54ab94b443e70e22e1620c
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-02-07 16:35:00 +0330

    Move player csv convertor to telegram bot.

--- telegram_bot/plugins/scripts.py
+++ telegram_bot/plugins/scripts.py
@@ -1,12 +1,25 @@
+import csv
 from pyrogram import filters
 from pyrogram.client import Client
 from pyrogram.types import Message
+from tournament.models import Player
 from custom_filters import admin_filter
-from utils.scripts import get_player_csv
 
 
-@Client.on_message(filters.private & admin_filter)
+@Client.on_message(filters.private & filters.regex("^/player_csv$") & admin_filter)
 def get_player_csv_file(client: Client, message: Message):
-    message.reply_text("Please wait ...")
-    get_player_csv()
+    msg = message.reply_text("Please wait ...")
+    with open("players.csv", "w") as csv_file:
+        field_names = ["id", "full_name", "telegram_id", "team_id", "team_name", "team_region"]
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
+        writer.writerow(field_names)
+
+        for player in Player.objects.all():
+            writer.writerow([player.pk, player.full_name, player.telegram_id, player.team_id, player.team_name, player.team_region])
+
+    msg.delete()
+    client.send_document(
+        message.chat.id,
+        "players.csv"
+    )
